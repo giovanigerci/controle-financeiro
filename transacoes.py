b1ed0a44949c11ee2f23ec502e450a86151893ad
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-06-06 13:24:27 -0300

    refactor: remove repeticao de codigo com decorator com_conexao

--- transacoes.py
+++ transacoes.py
@@ -1,85 +1,58 @@
-from database import conectar
-from mysql.connector import Error
+from database import com_conexao
 
 
-def adicionar_transacao(tipo, categoria, valor, descricao, data):
-    try:
-        with conectar() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('''
-                            INSERT INTO transacoes (tipo, categoria, valor, descricao, data)
-                            VALUES (%s , %s , %s , %s , %s)
-                            ''', (tipo, categoria, valor, descricao, data))
-                conn.commit()
-    except Error as e:
-        print(f'Erro ao acessar o banco: {e}')
+@com_conexao
+def adicionar_transacao(cursor, tipo, categoria, valor, descricao, data):
+    cursor.execute('''
+                INSERT INTO transacoes (tipo, categoria, valor, descricao, data)
+                VALUES (%s , %s , %s , %s , %s)
+                ''', (tipo, categoria, valor, descricao, data))
 
 
-def buscar_transacao_por_id(id_transacao):
-    try:
-        with conectar() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('''SELECT * FROM transacoes WHERE id = %s''', (id_transacao,))
-                return cursor.fetchone()
-    except Error as e:
-        print(f'Erro ao acessar o banco: {e}')
+@com_conexao
+def buscar_transacao_por_id(cursor, id_transacao):
+    cursor.execute('''SELECT * FROM transacoes WHERE id = %s''', (id_transacao,))
+    return cursor.fetchone()
 
 
-def listar_transacoes():
-    try:
-        with conectar() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('''SELECT * FROM transacoes''')
-                return cursor.fetchall()
-    except Error as e:
-        print(f'Erro ao acessar o banco: {e}')
+@com_conexao
+def listar_transacoes(cursor):
+    cursor.execute('''SELECT * FROM transacoes''')
+    return cursor.fetchall()
 
 
-def atualizar_transacao(id_transacao, nome_campo, novo_valor):
-    try:
-        with conectar() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(f'''
-                               UPDATE transacoes SET {nome_campo} = %s WHERE id = %s
-                               ''', (novo_valor, id_transacao))
-                conn.commit()
-                if cursor.rowcount == 0:
-                    return False
-                else:
-                    return True
-    except Error as e:
-        print(f'Erro ao acessar o banco: {e}')
+@com_conexao
+def atualizar_transacao(cursor, id_transacao, nome_campo, novo_valor):
+    cursor.execute(f'''
+                    UPDATE transacoes SET {nome_campo} = %s WHERE id = %s
+                    ''', (novo_valor, id_transacao))
+    if cursor.rowcount == 0:
+        return False
+    else:
+        return True
 
 
-def deletar_transacao(id_transacao):
-    try:
-        with conectar() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('''
-                               DELETE FROM transacoes WHERE id = %s
-                               ''', (id_transacao,))
-                conn.commit()
-                if cursor.rowcount == 0:
-                    return False
-                else:
-                    return True
-    except Error as e:
-        print(f'Erro ao acessar o banco: {e}')
+@com_conexao
+def deletar_transacao(cursor, id_transacao):
+    cursor.execute('''
+                    DELETE FROM transacoes WHERE id = %s
+                    ''', (id_transacao,))
+    if cursor.rowcount == 0:
+        return False
+    else:
+        return True
 
-def resumo_financeiro():
-    try:
-        with conectar() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('''
-                               SELECT SUM(valor) FROM transacoes WHERE tipo = %s
-                               ''', ('Receita',))
-                total_receita = cursor.fetchone()[0] or 0
 
-                cursor.execute('''
-                               SELECT SUM(valor) FROM transacoes WHERE tipo = %s
-                               ''', ('Despesa',))
-                total_despesa = cursor.fetchone()[0] or 0
-                saldo = total_receita - total_despesa
-                return total_receita, total_despesa, saldo
-    except Error as e:
-        print(f'Erro ao acessar o banco: {e}')
+@com_conexao
+def resumo_financeiro(cursor):
+    cursor.execute('''
+                    SELECT SUM(valor) FROM transacoes WHERE tipo = %s
+                    ''', ('Receita',))
+    total_receita = cursor.fetchone()[0] or 0
+
+    cursor.execute('''
+                    SELECT SUM(valor) FROM transacoes WHERE tipo = %s
+                    ''', ('Despesa',))
+    total_despesa = cursor.fetchone()[0] or 0
+    saldo = total_receita - total_despesa
+    return total_receita, total_despesa, saldo
